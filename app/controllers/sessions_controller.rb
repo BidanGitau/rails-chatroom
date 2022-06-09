87e5601d853bd12acd86cb285407a518f18552e0
BidanGitau <bydangitau@gmail.com>
2022-06-09 09:36:57 +0300

    adding activecable logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,32 @@
 class SessionsController < ApplicationController
-
+before_action :logged_in_redirect , only: [:new,:create]
 
   def new
   end
 
   def create
   user=User.find_by(username:params[:session][:username])
-  if user && user.authenticate(params[:session][:password_digest])
+  if user && user.authenticate(params[:session][:password])
+    session[:user_id]=user.id
     flash[:notice]="You logged in success"
-    redirect_to user
+    redirect_to root_path
   else
-    render 'new'
+
+    flash[:now]="Enter full details"
+      render 'new'
   end
   end
 
   def destroy
-
+    session[:user_id]=nil
+    flash[:error]="You were logged out"
+    redirect_to root_path, status: :see_other
   end
-
-
+ private
+ def logged_in_redirect
+   if logged_in?
+     flash[:error]="your already logged in"
+     redirect_to root_path
+ end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get "login" , to: "sessions#new"
   post "login" , to: "sessions#create"
   get "signup" , to: "users#new"
+  delete "logout" , to: "sessions#destroy"
   resources :users, except: [:new]
+  post "message" ,to: "messages#create"
   # Defines the root path route ("/")
     root "chatroom#index"
+    mount ActionCable.server,at: '/cable'
 end
